07dca089341fcf8ee95adebf031f8128be61de8e
RosieOh <20172207@gm.hannam.ac.kr>
2024-05-28 18:08:45 +0900

    ✨FEAT : Header 로직 구현-진행중

---
+++ mini-project-shop/src/layouts/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <nav class="navbar navbar-expand-1g navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Soldout</a>
+            <button class="navbar-toggle" type="button"></button>
+        </div>
+    </nav>
+</template>
