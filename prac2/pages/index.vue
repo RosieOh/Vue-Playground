754eb99db883a43f3eb5b7594362530172b23992
RosieOh <20172207@gm.hannam.ac.kr>
2024-06-23 02:10:56 +0900

    ✨FEAT : Nuxt.js 식 데이터 바인딩 연습

---
+++ prac2/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>Hello World!</div>
+    <button @click="show = !show">배열표시</button>
+    <ul v-if="show">
+      <li v-for="(item, i) in arr" :key="i">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data:() => ({
+    arr: [
+      '사과',
+      '바나나',
+      '참외',
+      '수박',
+    ],
+    show: true,
+  })
+}
+</script>
