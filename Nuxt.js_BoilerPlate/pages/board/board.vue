dd6ea184cd084537661a6a7da34bc362e3fe62ca
RosieOh <20172207@gm.hannam.ac.kr>
2024-06-23 02:07:51 +0900

    ✨BUILD : Nuxt.js BoilerPlate 제작 빌드

---
+++ Nuxt.js_BoilerPlate/pages/board/board.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-layout column>
+        <v-flex class="mt-4 ml-9">
+            <v-row>
+                <v-col cols="12" align="start">
+                    <h2>{{ pageName }}</h2>
+                </v-col>
+            </v-row>
+        </v-flex>
+
+        <v-flex class="ma-4" style="overflow:auto">
+            <v-row class="px-4">
+                <v-col cols="12">
+                    <v-card raised outlined class="pa-3">
+                        <list-table :list="list"/>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    import listTable from '~/components/board/listTable'
+
+    export default {
+        data() {
+            return {
+                pageName: 'index'
+            }
+        },
+
+        created() {
+            this.$store.dispatch('board/initBoardList');
+        },
+        computed: {
+            list() {
+                return this.$store.getters['board/GET_BOARD_LIST'];
+            }
+        },
+        methods: {
+            showDetail(id) {
+                console.log('showDetail : ', id);
+                this.$router.push(`${id}`);
+            }
+        },
+        components: {
+            listTable
+        }
+    }
+</script>
+
+<style>
+    img {
+        width: 100px;
+        height: 100px;
+    }
+
+    th, td {
+        text-align: center;
+        vertical-align: middle !important;
+    }
+</style>
