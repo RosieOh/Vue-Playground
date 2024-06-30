7c58fba6de2da79865bcea4a6067062e11f93711
RosieOh <20172207@gm.hannam.ac.kr>
2024-06-30 17:28:39 +0900

    ✨CHORE : 라우팅 경로 수정

--- mediring/src/main.js
+++ mediring/src/main.js
@@ -1,14 +1,10 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+const app = createApp(App);
 
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.use(router);
+app.use(store);
+app.mount('#app');
