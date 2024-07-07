9593eafba0e70e009856566bb1c2b38f0ff235e3
RosieOh <20172207@gm.hannam.ac.kr>
2024-07-07 22:50:34 +0900

    ✨FEAT : Computed 객체 활용

---
+++ Vue문법연습/ch05/src/views/Computed.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Full Name : {{fullName}}</h1>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        firstName: 'Taehoon',
+        lastName: 'Oh'
+      };
+    },
+    computed: {
+      fullName() {
+        return this.firstName + ' ' + this.lastName;
+      }
+    }
+  }
+</script>
